1f2909314d69d45e6d95b79d5614ddd6447108fa
Hos9 <shahin9315@gmail.com>
2026-03-29 15:28:20 +0600

    Button rendering done

--- src/Components/HomePage/Players/Players.jsx
+++ src/Components/HomePage/Players/Players.jsx
@@ -1,5 +1,6 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import AvailablePlayers from "../../AvaialablePlayers/AvailablePlayers";
+import SelectedPlayers from "./SelectedPlayers/SelectedPlayers";
 
 const Players = ({ playersPromise }) => {
   console.log(playersPromise);
@@ -7,10 +8,39 @@ const Players = ({ playersPromise }) => {
   const players = use(playersPromise);
   console.log(players);
 
+  const [selectedType, setSelectedType] = useState("available");
+  console.log(selectedType, "selectedType");
+
   return (
-    <div className=" max-w-11/12 mx-auto">
-      Players: {players.length}
-      <AvailablePlayers players={players}></AvailablePlayers>
+    <div className="max-w-11/12 mx-auto">
+      <div className="flex justify-between items-center my-5">
+        {selectedType === "available" ? (
+          <h2 className="font-bold text-xl">Available Players</h2>
+        ) : (
+          <h2 className="font-bold text-xl">Selected Player (4/6)</h2>
+        )}
+
+        <div>
+          <button
+            onClick={() => setSelectedType("available")}
+            className={`btn rounded-l-xl ${selectedType === "available" ? "bg-[#E7FE29]" : ""}`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => setSelectedType("selected")}
+            className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : ""} rounded-r-xl`}
+          >
+            Selected (0)
+          </button>
+        </div>
+      </div>
+
+      {selectedType === "available" ? (
+        <AvailablePlayers players={players} />
+      ) : (
+        <SelectedPlayers />
+      )}
     </div>
   );
 };
